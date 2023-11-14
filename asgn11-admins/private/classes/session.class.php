a1b70944e567e8d5f0f0333b911fb6630d963d09
skingsley <scott@example.com>
2023-11-14 17:25:12 -0500

    finished 5-9

--- asgn11-admins/private/classes/session.class.php
+++ asgn11-admins/private/classes/session.class.php
@@ -56,6 +56,18 @@ class Session {
     }
   }
 
+  public function message($msg="") {
+    if(!empty($msg)) {
+      $_SESSION['message'] = $msg;
+      return true;
+    } else {
+      return $_SESSION['message'] ?? '';
+    }
+  }
+
+  public function clear_message() {
+    unset($_SESSION['message']);
+  }
 }
 
 ?>
